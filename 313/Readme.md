12678a508f3e009b589ce83bc858709750b1173d
Bryanskiy <ivakin.ki@physteh.edu>
2021-02-27 15:55:04 +0300

    add latex links

---
+++ 313/Readme.md
@@ -0,0 +1,1 @@
+LATEX: https://www.overleaf.com/read/pmkncxbyfpfc
--- 431/main.py
+++ 431/main.py
@@ -47,26 +47,32 @@ def main():
     D = 216 ## mikro
     m = np.array([1, 2, 3, 4, 5, 6])
     a = np.array([68, 66, 63, 60, 55, 48]) # mili 
-    _2zm = np.sqrt(m * a * _lambda * 10**(-6)) #mili
+    _2zm = 2 * np.sqrt(m * a * _lambda * 10**(-6)) #mili
     sigma_2zm = np.array([])
     for i in range(_2zm.size):
         sigma_2zm = np.append(sigma_2zm, sigma_coss(_2zm[i], [2], [a[i]], [0.5]))
 
+    print (sigma_2zm)
+
     ## PLOT
     mpl.rcParams['font.size'] = 16
     plt.figure(figsize=(16,9))
-    plt.plot(m, _2zm, 'o', markersize=8)
+    plt.xlabel("m")
+    plt.plot(m, _2zm, 'o' ,label = "width of Frenel zones, mm", markersize=8)
     D_arr = [D/(10**(3))] * 6
-    plt.plot(m, D_arr)
+    plt.plot(m, D_arr, label = "width of gap, mm")
+    plt.errorbar(m, _2zm, yerr = sigma_2zm, fmt = '.')
     plt.minorticks_on()
     plt.grid(which='major', axis='both')
     plt.grid(which='minor', axis='both', linestyle = ':')
+    plt.legend()
     plt.savefig("frenel")
 
     # ----------------FRAINH------------------------------------------ 
     F2 = 12.8 # santi
     m = np.array([-4, -3, -2, -1, 1, 2, 3, 4])
     xm = np.array([-0.83, -0.56, -0.35, -0.20, 0.22, 0.38, 0.55, 0.80])
+    xm_error = np.array([0.01] * 8)
 
     k = k_coef(m, xm)
     b = b_coef(m, xm)
@@ -78,8 +84,11 @@ def main():
     ## PLOT
     mpl.rcParams['font.size'] = 16
     plt.figure(figsize=(16,9))
+    plt.xlabel("m")
+    plt.ylabel("x, mm")    
     plt.plot(m, xm, 'o', markersize=8)
     plt.plot(x_approx, y_approx, 'o', markersize=1)
+    plt.errorbar(m, xm, yerr = xm_error, fmt = '.')
     plt.minorticks_on()
     plt.grid(which='major', axis='both')
     plt.grid(which='minor', axis='both', linestyle = ':')
